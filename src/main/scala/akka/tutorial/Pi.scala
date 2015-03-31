96e077f16c7319ff9782608b8611c73baad469cb
bkpathak <bkpathak@mtu.edu>
2015-03-30 18:29:11 -0700

    minor fix

--- src/main/scala/akka/tutorial/Pi.scala
+++ src/main/scala/akka/tutorial/Pi.scala
@@ -1,78 +1,77 @@
 package akka.tutorial
 
-import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import akka.actor.{ActorSystem, Props, ActorRef, Actor}
 import akka.routing.RoundRobinRouter
-import akka.util.Duration
-import akka.util.duration._
+import scala.concurrent.duration._
+
 
 /**
  * Created by bijay on 3/30/15.
+ * Example tutorial from akka.io
  */
-sealed trait PiMessage
-
-case object Calculate extends PiMessage
-
-case class Work(start: Int, nrOfElements: Int) extends PiMessage
-
-case class Result(value: Double) extends PiMessage
-
-case class PiApproximation(pi: Double, duration: Duration)
-
-class Worker extends Actor {
-
-  // imperative implementation
-  def calculatePiFor(start: Int, nrOfElements: Int): Double = {
-    var acc = 0.0
-    for (i ← start until (start + nrOfElements))
-      acc += 4.0 * (1 - (i % 2) * 2) / (2 * i + 1)
-    acc
-  }
-
-  def receive = {
-    case Work(start, nrOfElements) ⇒
-      sender ! Result(calculatePiFor(start, nrOfElements)) // perform the work
-  }
-
-}
-
-class Master(nrOfWorkers: Int, nrOfMessages: Int, nrOfElements: Int, listener: ActorRef)
-  extends Actor {
-  var pi: Double = _
-  var nrOfResults: Int = _
-  val start: Long = System.currentTimeMillis
-  val workerRouter = context.actorOf(
-    Props[Worker].withRouter(RoundRobinRouter(nrOfWorkers)), name = "workerRouter")
-
-  def receive = {
-    case Calculate ⇒
-      for (i ← 0 until nrOfMessages) workerRouter ! Work(i * nrOfElements, nrOfElements)
-    case Result(value) ⇒
-      pi += value
-      nrOfResults += 1
-      if (nrOfResults == nrOfMessages) {
-        // Send the result to the listener
-        listener ! PiApproximation(pi, duration = (System.currentTimeMillis - start).millis)
-        // Stops this actor and all its supervised children
-        context.stop(self)
-      }
-  }
-
-}
-
-class Listener extends Actor {
-  def receive = {
-    case PiApproximation(pi, duration) ⇒
-      println("\n\tPi approximation: \t\t%s\n\tCalculation time: \t%s"
-        .format(pi, duration))
-      context.system.shutdown()
-  }
-}
-
 object Pi extends App {
 
   calculate(nrOfWorkers = 4, nrOfElements = 10000, nrOfMessages = 10000)
 
-  // actors and messages ...
+  sealed trait PiMessage
+
+  case object Calculate extends PiMessage
+
+  case class Work(start: Int, nrOfElements: Int) extends PiMessage
+
+  case class Result(value: Double) extends PiMessage
+
+  case class PiApproximation(pi: Double, duration: Duration)
+
+  class Worker extends Actor {
+
+    // imperative implementation
+    def calculatePiFor(start: Int, nrOfElements: Int): Double = {
+      var acc = 0.0
+      for (i ← start until (start + nrOfElements))
+        acc += 4.0 * (1 - (i % 2) * 2) / (2 * i + 1)
+      acc
+    }
+
+    def receive = {
+      case Work(start, nrOfElements) ⇒
+        sender ! Result(calculatePiFor(start, nrOfElements)) // perform the work
+    }
+
+  }
+
+  class Master(nrOfWorkers: Int, nrOfMessages: Int, nrOfElements: Int, listener: ActorRef)
+    extends Actor {
+    var pi: Double = _
+    var nrOfResults: Int = _
+    val start: Long = System.currentTimeMillis
+    val workerRouter = context.actorOf(
+      Props[Worker].withRouter(RoundRobinRouter(nrOfWorkers)), name = "workerRouter")
+
+    def receive = {
+      case Calculate ⇒
+        for (i ← 0 until nrOfMessages) workerRouter ! Work(i * nrOfElements, nrOfElements)
+      case Result(value) ⇒
+        pi += value
+        nrOfResults += 1
+        if (nrOfResults == nrOfMessages) {
+          // Send the result to the listener
+          listener ! PiApproximation(pi, duration = (System.currentTimeMillis - start).millis)
+          // Stops this actor and all its supervised children
+          context.stop(self)
+        }
+    }
+
+  }
+
+  class Listener extends Actor {
+    def receive = {
+      case PiApproximation(pi, duration) ⇒
+        println("\n\tPi approximation: \t\t%s\n\tCalculation time: \t%s"
+          .format(pi, duration))
+        context.system.shutdown()
+    }
+  }
 
   def calculate(nrOfWorkers: Int, nrOfElements: Int, nrOfMessages: Int) {
     // Create an Akka system
@@ -91,4 +90,3 @@ object Pi extends App {
 
   }
 }
-
